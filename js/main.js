af0032335463d23803243e47e9ebe09f24108a79
Danna E Wroblewski <danna.basschic@gmail.com>
2018-04-27 00:05:12 -0700

    added hideMe +

--- js/main.js
+++ js/main.js
@@ -8,6 +8,33 @@ $(document).ready(function() {
     var jumboScroll = $('.jumbotron').scrollTop();
     console.log('hello');
   }
+  $('.cover_page').click(function() {
+    $('#newsPaper').fadeIn('slow', function() {
+      //Animation complete
+      console.log("please work!");
+    });
+    $('.cover_page').hide('fast', function(){
+
+    });
+  });
+  $('#hideMe').click(function() {
+    $('#newsPaper').hide('fast', function() {
+      console.log("One");
+    });
+    $('.cover_page').hide('fast', function() {
+      console.log("One.Five");
+    });
+    $('#all_done').show('fast', function() {
+      console.log("Two");
+    });
+    $('#all_done').delay(800).hide('fast', function() {
+      console.log("Three");
+
+    });
+    $('#white').show('fast', function() {
+      console.log("For the love of everything work!!");
+    });
+  });
 
   // function changeImage(img) {
   //   // $('.crups').click(function(){
@@ -18,30 +45,28 @@ $(document).ready(function() {
   //   //   }
   //   // });
   var dogPic2 = 0;
-    $('.crups').click(function() {
-      console.log("It worked, maybe?")
-      if(dogPic2===0){
-        var dogPic = $(this).attr('data-alt-src');
-        dogPic2 = 1;
-      }
-      else {
-        var dogPic =$(this).attr('data-alt-src2');
-        dogPic2 = 0;
-      }
-      $(this).attr('src', dogPic);
-    });
-    var spaceBroom = 0;
-      $('.tesla').hover(function() {
-        console.log("It worked, maybe?")
-        if(spaceBroom===0){
-          var tesla = $(this).attr('data-alt-src');
-          spaceBroom = 1;
-        }
-        else {
-          var tesla =$(this).attr('data-alt-src2');
-          spaceBroom = 0;
-        }
-        $(this).attr('src', tesla);
-      });
+  $('.crups').click(function() {
+    console.log("It worked, maybe?")
+    if (dogPic2 === 0) {
+      var dogPic = $(this).attr('data-alt-src');
+      dogPic2 = 1;
+    } else {
+      var dogPic = $(this).attr('data-alt-src2');
+      dogPic2 = 0;
+    }
+    $(this).attr('src', dogPic);
+  });
+  var spaceBroom = 0;
+  $('.tesla').hover(function() {
+    console.log("It worked, maybe?")
+    if (spaceBroom === 0) {
+      var tesla = $(this).attr('data-alt-src');
+      spaceBroom = 1;
+    } else {
+      var tesla = $(this).attr('data-alt-src2');
+      spaceBroom = 0;
+    }
+    $(this).attr('src', tesla);
+  });
   // }
 });
